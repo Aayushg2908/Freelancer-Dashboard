87c4ecb5b875e94049e1f2284990b38e6e0bf69a
Aayush <aayushg2908@gmail.com>
2024-03-26 16:17:26 +0530

    feat: adding test users for authentication

--- app/(dashboard)/clients/loading.tsx
+++ app/(dashboard)/clients/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 const loading = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full mt-32 flex items-center justify-center">
       <Loader2 className="animate-spin" size={64} />
     </div>
   );
